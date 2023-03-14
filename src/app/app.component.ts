2619c08544e0f79de00e953e3ee942a53b728b1a
Pratik Jagadale <jagadalepratik54@gmail.com>
2023-03-14 19:24:41 +0530

    Project Tab Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 
 @Component({
@@ -7,5 +7,34 @@ import { HomeComponent } from './home/home.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('sidebar')
+  mySidebar!: ElementRef;
+
+  element!: HTMLElement;
+  element1!: HTMLElement;
+
   title = 'MatrixInnovationsIndia';
+
+  constructor() {
+    this.element = document.getElementById('sidebar') as HTMLElement;
+  }
+
+  openNav() {
+    this.element = (document.getElementById('innerId')) as HTMLElement;
+    this.element.style.width = "0px";
+
+    this.element1 = (document.getElementById('sidebar')) as HTMLElement;
+    this.element1.style.width = "0px";
+    console.log("openNav clicked..");
+
+    //document.getElementById("mySidebar").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  closeNav() {
+    this.element.style.width = "0px";
+    //document.getElementById("mySidebar").style.width = "0";
+    ///document.getElementById("main").style.marginLeft = "0";
+  }
+
 }
