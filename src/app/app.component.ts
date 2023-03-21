04cbba5d7e17436e558fcd66b30c555f7718c658
Pratik-Jagadale <jagadalepratik54@gmail.com>
2023-03-21 14:40:18 +0530

    Changes for android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,38 @@ import { HomeComponent } from './home/home.component';
 export class AppComponent {
   title = 'MatrixInnovationsIndia';
 
-  public show: boolean = true;
-  public notToShow: boolean = false;
-  public mobile: boolean = false;
-
-  public details = navigator.userAgent;
-  public regexpAndroid = new RegExp('android*');
-  public regexpIPhone = new RegExp('iphone*');
-  public regexpIPad = new RegExp('ipad*');
-  public regexpLinux = new RegExp('Linux*');
-
+  public showHeader: boolean = false;
+  public showMain: boolean = true;
+  /*
+    public details = navigator.userAgent;
+    public regexpAndroid = new RegExp('android*');
+    public regexpIPhone = new RegExp('iphone*');
+    public regexpIPad = new RegExp('ipad*');
+    public regexpLinux = new RegExp('Linux*');
+  */
   public isMobileDevice: boolean = true;
   public isWindowsOrLinuxeDevice: boolean = false;
 
   constructor() {
 
-    if (this.regexpAndroid.test(this.details) || this.regexpIPhone.test(this.details) || this.regexpIPad.test(this.details)) {
-      // returns true if working locally
+    if (navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/iPhone/i)) {
+
+
       this.isMobileDevice = true;
       this.isWindowsOrLinuxeDevice = false;
+
+      this.showHeader = false;
+      this.showMain = true;
+
       console.log('This is Android Device\n');
     }
     else {
+
+      console.log('This is Not Android Device\n');
+
+      this.showHeader = true;
+      this.showMain = true;
 
       this.isMobileDevice = false;
       this.isWindowsOrLinuxeDevice = true;
@@ -40,25 +50,24 @@ export class AppComponent {
   }
 
   openNav() {
-    this.show = !this.show;
-    this.notToShow = !this.notToShow;
+    this.showHeader = !this.showHeader;
+    this.showMain = !this.showMain;
 
-    if (this.regexpAndroid.test(this.details) || this.regexpIPhone.test(this.details) || this.regexpIPad.test(this.details)) {
-      //returns true if working locally
-      //this.isMobileDevice = true;
 
-      console.log('This is Android Device\n');
+    console.log('this.show =', this.showHeader);
+    console.log('this.notToShow', this.showMain);
 
-    }
-    console.log('this.show =', this.show);
-    console.log('this.notToShow', this.notToShow);
+  }
+
+  tabChanged() {
+    this.showHeader = false;
+    this.showMain = true;
 
   }
 
   ngOnInit() {
     if (window.screen.width === 360) { // 768px portrait
-      this.mobile = true;
-      console.log(this.mobile);
+
     }
   }
 
